321b09fca2caaef36d4487efe82f986ac1f489ae
RisingStar522 <brianyoung.luck@gmail.com>
2022-06-25 03:16:04 -0400

    first commit for testing

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    theme: {
+      fontWeight: {
+        thin: 100,
+        extralight: 200,
+        light: 300,
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        black: 900,
+      }
+    }
+  },
+  plugins: [],
+}
